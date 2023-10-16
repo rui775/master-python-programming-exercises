3e5f3eac1b65c750e4ed21d9286abfd6c23d7af6
rui775 <rsilva6420@gmail.com>
2023-10-16 14:44:35 +0100

    exercise 15 completed

--- .learn/resets/013-sum_of_digits/app.py
+++ .learn/resets/013-sum_of_digits/app.py
@@ -1,9 +1,6 @@
 #Complete the function "digits_sum" so that it prints the sum of a three digit number.
 def digits_sum(num):
-  sum = 0
-  for i in str(num):
-    sum += int(i)
-  return sum
+  return None
 
 
 #Invoke the function with any three-digit-number
--- .learn/resets/014-digit_after_decimal_point/app.py
+++ .learn/resets/014-digit_after_decimal_point/app.py
@@ -1,9 +1,8 @@
 #Complete the function to return the first digit to the right of the decimal point.
 def first_digit(num):
-  my_str = str(num)
-  for i in range(len(my_str)):
-    if my_str[i] == ".":
-      return my_str[i+1]
+  return None
+
+
 
 #Invoke the function with a positive real number. ex. 34.33
-print(first_digit(1.79))
+print(first_digit())
