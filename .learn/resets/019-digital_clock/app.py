622f7774298a99a50b01db4e3b6502d9bbb5b6da
rsilv <rsilva6420@gmail.com>
2023-10-23 10:46:27 +0100

    exercise 20 completed

--- .learn/resets/019-digital_clock/app.py
+++ .learn/resets/019-digital_clock/app.py
@@ -1,9 +1,6 @@
 #Complete the function to return how many hrs and min are displayed on the 24th digital clock.
-import math
 def digital_clock(n):
-  hours = math.floor(n/60)
-  minutes = n%60
-  return hours, minutes
+  return None
 
 #Invoke the function with any interger (seconds after midnight)
-print(digital_clock(150))
+print(digital_clock())
--- exercises/018-day_of_week/app.py
+++ exercises/018-day_of_week/app.py
@@ -2,9 +2,9 @@
 def day_of_week(k):
   # the modulus of the number of the day divide by 7 (days of a week) will result in the days that ramain to complete the week 
   # therefore will give us the day of the week. Plus 3 because the year started on thursday (1 -> Monday + 3 = 4 -> Thursday)
-  return k % 7 + 3
+  return (k+3) % 7
 
 
 
 #Invoke function day_of_week with an interger between 0 and 6 (number for days of week)
-print(day_of_week(1))
+print(day_of_week(46))
